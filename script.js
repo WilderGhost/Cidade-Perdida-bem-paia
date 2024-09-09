6090555402608498ed2f8cc2bf6f8ef1617dc0b2
Falk <95051139+WilderGhost@users.noreply.github.com>
2024-09-09 11:41:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let botao = document.querySelector('.meu-botao');
+const avanca = document.querySelectorAll('.btn-proximo');
+
+
+avanca.forEach(button =>{
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+
+
+
+})
